9f732af8ab9730397aacf883b2c43d533ce836ec
Jamie <phillipsja97@gmail.com>
2019-12-09 20:42:06 -0600

    added proptypes

--- src/components/GoatCorral/GoatCorral.js
+++ src/components/GoatCorral/GoatCorral.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Goat from '../Goat/Goat';
+import goatShape from '../../helpers/propz/goatShape';
 
 class GoatCorral extends React.Component {
+  static propTypes = {
+    goats: PropTypes.arrayOf(goatShape.goatShape),
+  }
+
   render() {
     const myGoats = this.props.goats;
 
